0542ba8e05d8012a2b8f4acdcbda1fb383260e5f
Maximilian Goisser <goisser94@gmail.com>
2019-06-03 11:44:06 +0200

    feat(seed): add function to sort entities by transitive dependencies

--- src/commands/seed/utils.js
+++ src/commands/seed/utils.js
@@ -1,6 +1,27 @@
 const ontology = require("@rlay/ontology");
 
 /**
+ * Check wether a entity field is a "special" field.
+ * "Special" means all fields where the value is not assumed to be a CID.
+ */
+const isSpecialField = (entity, field) => {
+  if (field === "type") {
+    return true;
+  }
+  if (field === "value" && entity.type === "Annotation") {
+    return true;
+  }
+  if (
+    field === "target" &&
+    (entity.type === "DataPropertyAssertion" ||
+      entity.type === "NegativeDataPropertyAssertion")
+  ) {
+    return true;
+  }
+  return false;
+};
+
+/**
  * Takes a map where the keys are entity reference names, and the values are entities.
  * The entities can have references to other entities in the form of`*<REFERENCE_NAME>` in place
  * of CIDs.
@@ -8,23 +29,6 @@ const ontology = require("@rlay/ontology");
  * Outputs a map with the same keys, where all the references have been replaced with calculated CIDs.
  */
 const calculateEntityTreeReferences = entityTree => {
-  const isSpecialField = (entity, field) => {
-    if (field === "type") {
-      return true;
-    }
-    if (field === "value" && entity.type === "Annotation") {
-      return true;
-    }
-    if (
-      field === "target" &&
-      (entity.type === "DataPropertyAssertion" ||
-        entity.type === "NegativeDataPropertyAssertion")
-    ) {
-      return true;
-    }
-    return false;
-  };
-
   const cidMap = {};
   const derefTree = {};
 
@@ -112,6 +116,66 @@ const calculateEntityTreeReferences = entityTree => {
   return derefTree;
 };
 
+/**
+ * Takes an array of entities, and returns an array of entities
+ * sorted by their number of transitive dependencies (least dependencies first).
+ */
+const sortByDependencyCount = entitiesIn => {
+  const countMap = {};
+  const cidEntityMap = {};
+
+  const lookupCidDepCount = cid => {
+    let count = countMap[cid];
+    if (count) {
+      return count;
+    }
+
+    const entity = cidEntityMap[cid];
+    if (!entity) {
+      return 1;
+    }
+
+    count = countDependencies(entity);
+    return count;
+  };
+
+  const countDependencies = entity => {
+    let count = 1;
+
+    Object.keys(entity).forEach(field => {
+      if (isSpecialField(entity, field)) {
+        return;
+      }
+
+      const fieldVal = entity[field];
+      if (Array.isArray(fieldVal)) {
+        fieldVal.forEach(val => {
+          count += lookupCidDepCount(val);
+        });
+      } else {
+        count += lookupCidDepCount(fieldVal);
+      }
+    });
+
+    return count;
+  };
+
+  entitiesIn.forEach(entity => {
+    const cid = ontology.getEntityCid(entity);
+    cidEntityMap[cid] = entity;
+  });
+
+  const entitiesWithCount = entitiesIn.map(entity => ({
+    entity,
+    depCount: countDependencies(entity)
+  }));
+  entitiesWithCount.sort((a, b) => a.depCount - b.depCount);
+
+  const entities = entitiesWithCount.map(n => n.entity);
+  return entities;
+};
+
 module.exports = {
-  calculateEntityTreeReferences
+  calculateEntityTreeReferences,
+  sortByDependencyCount
 };
--- test/seedfile.spec.js
+++ test/seedfile.spec.js
@@ -1,51 +1,96 @@
 const assert = require("assert");
 
-const { calculateEntityTreeReferences } = require("../src/commands/seed/utils");
+const {
+  calculateEntityTreeReferences,
+  sortByDependencyCount
+} = require("../src/commands/seed/utils");
 
 describe("Seed Utils", () => {
-  it("should not modify tree without references", () => {
-    const entityTree = {
-      classRef: {
-        type: "Class"
-      },
-      indRef: {
-        type: "Individual"
-      }
-    };
+  describe("calculateEntityTreeReferences", () => {
+    it("should not modify tree without references", () => {
+      const entityTree = {
+        classRef: {
+          type: "Class"
+        },
+        indRef: {
+          type: "Individual"
+        }
+      };
 
-    const calculatedTree = calculateEntityTreeReferences(entityTree);
-    assert.deepEqual(calculatedTree["classRef"], {
-      type: "Class"
+      const calculatedTree = calculateEntityTreeReferences(entityTree);
+      assert.deepEqual(calculatedTree["classRef"], {
+        type: "Class"
+      });
+      assert.deepEqual(calculatedTree["indRef"], {
+        type: "Individual"
+      });
     });
-    assert.deepEqual(calculatedTree["indRef"], {
-      type: "Individual"
+
+    it("should calculate a simple reference CID correctly", () => {
+      const entityTree = {
+        classRef: {
+          type: "Class"
+        },
+        indRef: {
+          type: "Individual"
+        },
+        classAssertRef: {
+          type: "ClassAssertion",
+          subject: "*indRef",
+          class: "*classRef"
+        }
+      };
+
+      const calculatedTree = calculateEntityTreeReferences(entityTree);
+      assert.deepEqual(calculatedTree["classRef"], entityTree["classRef"]);
+      assert.deepEqual(calculatedTree["indRef"], entityTree["indRef"]);
+      assert.deepEqual(calculatedTree["classAssertRef"], {
+        type: "ClassAssertion",
+        subject:
+          "0x019680031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+        class:
+          "0x018080031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+      });
     });
   });
 
-  it("should calculate a simple reference CID correctly", () => {
-    const entityTree = {
-      classRef: {
-        type: "Class"
-      },
-      indRef: {
-        type: "Individual"
-      },
-      classAssertRef: {
-        type: "ClassAssertion",
-        subject: "*indRef",
-        class: "*classRef"
-      }
-    };
+  describe("sortByDependencyCount", () => {
+    it("should sort a few entities correctly", () => {
+      const entities = [
+        {
+          type: "ClassAssertion",
+          annotations: [
+            "0x1111111111111111111111111111111111111111111111111111111111111111111111111111"
+          ],
+          subject:
+            "0x019680031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+          class:
+            "0x018080031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+        },
+        {
+          type: "Class"
+        },
+        {
+          type: "ClassAssertion",
+          subject:
+            "0x019680031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
+          class:
+            "0x018080031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+        },
+        {
+          type: "Individual"
+        }
+      ];
 
-    const calculatedTree = calculateEntityTreeReferences(entityTree);
-    assert.deepEqual(calculatedTree["classRef"], entityTree["classRef"]);
-    assert.deepEqual(calculatedTree["indRef"], entityTree["indRef"]);
-    assert.deepEqual(calculatedTree["classAssertRef"], {
-      type: "ClassAssertion",
-      subject:
-        "0x019680031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
-      class:
-        "0x018080031b20c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+      const sortedEntities = sortByDependencyCount(entities);
+      assert.equal(sortedEntities[0].type, "Class");
+      assert.equal(sortedEntities[1].type, "Individual");
+      assert.equal(sortedEntities[2].type, "ClassAssertion");
+      assert.equal(sortedEntities[3].type, "ClassAssertion");
+      assert.equal(
+        sortedEntities[3].annotations[0],
+        "0x1111111111111111111111111111111111111111111111111111111111111111111111111111"
+      );
     });
   });
 });

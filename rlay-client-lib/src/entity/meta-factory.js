a3d916221fdd9b762468748928261111505845d1
Michael Hirn <michael.j.hirn@gmail.com>
2019-06-25 00:14:01 +0100

    fix/rlay-client-lib: fix individual properties bug

--- rlay-client-lib/src/entity/meta-factory.js
+++ rlay-client-lib/src/entity/meta-factory.js
@@ -38,7 +38,7 @@ class EntityMetaFactory {
       const oldStarReferenceRegex = /\[.\*|,.\*|:.\*/g;
       if (oldStarReferenceRegex.test(e.message)) {
         logger.warn('DEPRECATED WARNING: Update your schema files; you are using * references, which should be replaced with the actual CID of the entity. This will throw an error in the next MINOR version. No CID can be computed for this entity');
-        logger.info(`Original Error Message: ${e.message}`);
+        logger.error(`Original Error Message: ${e.message}`);
       } else {
         // rethrow
         throw e
@@ -55,17 +55,23 @@ class EntityMetaFactory {
       newClass[newClassName] = class extends this.client.Rlay_ClassAssertion { }
       newClass[newClassName].type = this.client.Rlay_ClassAssertion.type;
       newClass[newClassName].fields = this.client.Rlay_ClassAssertion.fields;
-      newClass[newClassName].fieldsDefault = { subject: '0x00', class: entity.cid };
+      newClass[newClassName].fieldsDefault = this.client.Rlay_ClassAssertion.fieldsDefault;
+      newClass[newClassName].fieldsDefault.subject = '0x00';
+      newClass[newClassName].fieldsDefault.class = entity.cid;
     } else if (entity instanceof this.client.Rlay_DataProperty) {
       newClass[newClassName] = class extends this.client.Rlay_DataPropertyAssertion { }
       newClass[newClassName].type = this.client.Rlay_DataPropertyAssertion.type;
       newClass[newClassName].fields = this.client.Rlay_DataPropertyAssertion.fields;
-      newClass[newClassName].fieldsDefault = { subject: '0x00', property: entity.cid };
+      newClass[newClassName].fieldsDefault = this.client.Rlay_DataPropertyAssertion.fieldsDefault;
+      newClass[newClassName].fieldsDefault.subject = '0x00';
+      newClass[newClassName].fieldsDefault.property = entity.cid;
     } else if (entity instanceof this.client.Rlay_ObjectProperty) {
       newClass[newClassName] = class extends this.client.Rlay_ObjectPropertyAssertion { }
       newClass[newClassName].type = this.client.Rlay_ObjectPropertyAssertion.type;
       newClass[newClassName].fields = this.client.Rlay_ObjectPropertyAssertion.fields;
-      newClass[newClassName].fieldsDefault = { subject: '0x00', property: entity.cid };
+      newClass[newClassName].fieldsDefault = this.client.Rlay_ObjectPropertyAssertion.fieldsDefault;
+      newClass[newClassName].fieldsDefault.subject = '0x00';
+      newClass[newClassName].fieldsDefault.property = entity.cid;
     } else {
       return undefined
       //return new Error(`Can not create Entity Class from entity with type: ${entity.type}`);
